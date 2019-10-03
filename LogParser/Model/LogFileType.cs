2ae9f65230d1892d1f55b0bd272fa618dbf9e980
Grishchenko Timofei <timoxa810@gmail.com>
2019-10-03 10:05:18 +0300

    add LogFileType model

--- LogParser/Model/LogFileType.cs
+++ LogParser/Model/LogFileType.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LogParser.Model
 {
-    class LogFileType
+    public class LogFileType : INotifyPropertyChanged
     {
+        private bool isCarriageWithVariableFrequencyDrive;
+        public bool IsCarriageWithVariableFrequencyDrive
+        {
+            get { return isCarriageWithVariableFrequencyDrive; }
+            set
+            {
+                if (isCarriageWithVariableFrequencyDrive != value)
+                {
+                    isCarriageWithVariableFrequencyDrive = value;
+                    OnPropertyChanged("IsCarriageWithVariableFrequencyDrive");
+                }
+            }
+        }
+
+        private bool isCarriageWithSoftStartup;
+        public bool IsCarriageWithSoftStartup
+        {
+            get { return isCarriageWithSoftStartup; }
+            set
+            {
+                if (isCarriageWithSoftStartup != value)
+                {
+                    isCarriageWithSoftStartup = value;
+                    OnPropertyChanged("IsCarriageWithSoftStartup");
+                }
+            }
+        }
+
+        #region Implement INotifyPropertyChanged members
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
     }
 }
--- LogParser/ViewModel/MainWindowViewModel.cs
+++ LogParser/ViewModel/MainWindowViewModel.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LogParser.Model;
 
 namespace LogParser.ViewModel
 {
     class MainWindowViewModel
     {
+        public static LogFileInformation LogFileInformation { get; set; }
+        public static LogFileType LogFileType { get; set; }
+
+        public MainWindowViewModel()
+        {
+            LogFileInformation = new LogFileInformation { };
+            LogFileType = new LogFileType { };
+
+            TestLogFileInformation();
+        }
+
+        public void TestLogFileInformation()
+        {
+            LogFileInformation.FileName = "1";
+            LogFileInformation.DataPeriod = "2";
+            LogFileInformation.NotesFromCarriageWithVariableFrequencyDrive = "3";
+            LogFileInformation.NotesFromCarriageWithSoftStartup = "4";
+            LogFileInformation.NumericData = "5";
+            LogFileInformation.EventsData = "6";
+        }
+
+        public void TestLogFileType()
+        {
+
+        }
+
+
     }
 }

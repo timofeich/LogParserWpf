15591bb478e5ff5d3317a08d3e803265d78db81b
Gomer Pyle <timoxa810@gmail.com>
2019-10-19 10:05:30 +0300

    droping EventAndTableDataModel

--- LogParser/ViewModel/TableAndEventDataViewModel.cs
+++ LogParser/ViewModel/TableAndEventDataViewModel.cs
@@ -7,41 +7,10 @@ namespace LogParser.ViewModel
     public class TableAndEventDataViewModel
     {
         public event EventHandler OnRequestClose;
-        string[] LogFileContent;
-        public ObservableCollection<TableAndEventData> TableAndEventDatas { get; set; }
-
-        public TableAndEventDataViewModel(string[] LogFileContent)
+        
+        public TableAndEventDataViewModel()
         {
-            TableAndEventDatas = new ObservableCollection<TableAndEventData> 
-            { 
-                new TableAndEventData { MessageID = 0, TextMessage = "123", MessageDate = "Steven" },
-                new TableAndEventData { MessageID = 1, TextMessage = "321", MessageDate = "John" },
-            };
 
-            this.LogFileContent = LogFileContent;
-
-            TableAndEventData tableAndEventData = new TableAndEventData();
-            OutputEventData(tableAndEventData);
-        }
-
-        private void OutputEventData(TableAndEventData tableAndEventData)
-        {
-            //tableAndEventData.MessageID = 2;
-            //tableAndEventData.TextMessage = LogFileContent[3];
-            //tableAndEventData.MessageDate = LogFileContent[0];
-            //tableAndEventData.TimeOfRequest = LogFileContent[2];
-            //tableAndEventData.VoltageA = Convert.ToInt32(LogFileContent[4]);
-            //tableAndEventData.VoltageB = Convert.ToInt32(LogFileContent[5]);
-            //tableAndEventData.VoltageC = Convert.ToInt32(LogFileContent[6]);
-            //tableAndEventData.AmperageA = Convert.ToInt32(LogFileContent[7]);
-            //tableAndEventData.AmperageB = Convert.ToInt32(LogFileContent[8]);
-            //tableAndEventData.AmperageC = Convert.ToInt32(LogFileContent[9]);
-            //tableAndEventData.Loil = Convert.ToInt32(LogFileContent[10]);
-            //tableAndEventData.Toil = Convert.ToInt32(LogFileContent[11]);
-            //tableAndEventData.Poil = Convert.ToInt32(LogFileContent[12]);
-            //tableAndEventData.ThyristorTemperature = Convert.ToInt32(LogFileContent[13]);
-
-            //TableAndEventDatas.Add(tableAndEventData);
         }
 
     }

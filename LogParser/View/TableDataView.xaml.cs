d1dad550b317595b03c5d08c8995b994b78a511d
Gomer Pyle <timoxa810@gmail.com>
2019-10-31 11:21:23 +0300

    add table to table redirection by date

--- LogParser/View/TableDataView.xaml.cs
+++ LogParser/View/TableDataView.xaml.cs
@@ -1,8 +1,12 @@
-﻿using LogParser.ViewModel;
+﻿using LogParser.Model;
+using LogParser.ViewModel;
 using System;
 using System.Globalization;
+using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Input;
 
 namespace LogParser.View
 {
@@ -11,11 +15,13 @@ namespace LogParser.View
     /// </summary>
     public partial class TableDataView : Window
     {
+        private EventJoinedWithTableData result { get; set; }
+
+        private DateTime dateOfEvent { get; set; }
         public TableDataView()
         {
             InitializeComponent();
         }
-        
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
@@ -26,5 +32,57 @@ namespace LogParser.View
                 FontSize = newSize;
             }
         }
+
+        private void SelectJoinedEventDateByTableDate(object sender, MouseButtonEventArgs e)
+        {
+            SearchValueInJoinedData(DataGridWithTableData, 1);            
+        }
+
+        private void SelectJoinedEventDateByEventDate(object sender, MouseButtonEventArgs e)
+        {
+            SearchValueInJoinedData(DataGridWithEventData, 2);
+        }
+
+        private void SearchValueInJoinedData(DataGrid tableDataView, int dateColumn)
+        {
+            var item = tableDataView.SelectedValue;
+            var content = (tableDataView.SelectedCells[dateColumn].Column.GetCellContent(item) as TextBox).Text;
+
+            dateOfEvent = Convert.ToDateTime(content);
+
+            result = JoinedEventData.Items.Cast<EventJoinedWithTableData>().FirstOrDefault(w => w.Date == dateOfEvent);
+
+            if (result == null)
+            {
+                SearchValueInJoinedDataWithInterval(0, 120);
+            }
+            else
+            {
+                FocusOnSearchResult();
+            }
+        }
+
+        private void SearchValueInJoinedDataWithInterval(int leftInterval, int rightInterval)
+        {
+            for (int i = leftInterval; i < rightInterval; i++)
+            {
+                result = JoinedEventData.Items.Cast<EventJoinedWithTableData>().FirstOrDefault(w => w.Date == dateOfEvent.AddSeconds(i));
+                if (result != null)
+                {
+                    FocusOnSearchResult();
+                    break;
+                }
+            }
+        }
+
+        private void FocusOnSearchResult()
+        {
+            TabItem3.IsSelected = true;
+
+            JoinedEventData.ScrollIntoView(JoinedEventData.Items[result.ID - 1]);
+
+            JoinedEventData.SelectedItem = JoinedEventData.Items[result.ID - 1];
+        }
+
     }
 }
